1e3ffe83c72e7780c6607f1e21d04a27931b43e1
kamaukimani <paulkimys285@gmail.com>
2025-06-08 19:30:26 +0300

    update chai.js

--- chai-calculator/chai.js
+++ chai-calculator/chai.js
@@ -1,20 +1,31 @@
+const readline = require('readline');
+
 const waterCup= 200;
 const milkCup= 50;
 const teaLeavesCup= 1;
 const sugarCup= 2;
-let numCups=3;
 
-function calculateChaiIngredients(numberOfCup) {
-    let numberOfCups=prompt("Enter the number of chai cups you want to make:",6);
-    water=waterCup *numberOfCups;
-    milk=milkCup * numCups;
-    tealeaves=teaLeavesCup * numCups;
-    sugar=sugarCup *numCups;
+function calculateChaiIngredients(numberOfCups) {
+    const water=waterCup *numberOfCups;
+    const milk=milkCup * numberOfCups;
+    const tealeaves=teaLeavesCup * numberOfCups;
+    const sugar=sugarCup *numberOfCups;
 
-}
-console.log("To make Kenyan chai, you will need:");
+
+console.log("To make"+" "+numberOfCups+" "+ "cups of Kenyan chai, you will need:");
 console.log("Water:"+water+" "+"ml");
 console.log("Milk:"+milk+" "+"ml");
-console.log("Teal Leaves:"+tealeaves+" "+"tablespoons");
-console.log("Sugar:"+water+" "+"teaspoons")
+console.log("Teal Leaves (Majani):"+tealeaves+" "+"tablespoons");
+console.log("Sugar:"+sugar+" "+"teaspoons")
 console.log("Enjoy your Chai Bora!")
+}
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+rl.question("Karibu! How many cups of Chai Bora would you like to make? ", (input) => {
+    const numberOfCups = parseFloat(input);
+    calculateChaiIngredients(numberOfCups);
+  rl.close();
+});
