93d825bb440aec02e302975c727d3dd40cf5f7d7
kamaukimani <paulkimys285@gmail.com>
2025-06-08 16:49:50 +0300

    add console

--- chai-calculator/chai.js
+++ chai-calculator/chai.js
@@ -3,12 +3,16 @@ const milkCup= 50;
 const teaLeavesCup= 1;
 const sugarCup= 2;
 let numCups=3;
-function calculateChaiIngredients(numberOfCups) {
+//function calculateChaiIngredients(numberOfCups) {
     water=waterCup *numCups;
     milk=milkCup * numCups;
     tealeaves=teaLeavesCup * numCups;
     sugar=sugarCup *numCups;
 
-}
-console.log("To make Kenyan chai, you will need:")
-console.log("Water:"+water+" "+"ml")
+//}
+console.log("To make Kenyan chai, you will need:");
+console.log("Water:"+water+" "+"ml");
+console.log("Milk:"+milk+" "+"ml");
+console.log("Teal Leaves:"+tealeaves+" "+"tablespoons");
+console.log("Sugar:"+water+" "+"teaspoons")
+console.log("Enjoy your Chai Bora!")
